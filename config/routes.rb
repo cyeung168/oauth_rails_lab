06c7cd38d19678675e2ba2ad375ed051f6bd1a00
Cynthia Yeung <cyeung91@gmail.com>
2015-03-03 00:30:23 -0800

    Completed Twitter Oauth

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :site
+  root 'site#index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+end
